d2b441a870fb663f96ad48c473db4e08b75fed96
Adam Cardilini <a.cardilini@gmail.com>
2021-08-15 17:09:11 +1000

    Add project

---
+++ Eggsperience/Assets/Altspace/Export Assets/Editor/UserPreferences.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class UserPreferences : ScriptableObject {
+
+    public string ProjectName;
+    public string ExportPath;
+    public bool IsWindowsBuild;
+    public bool IsAndroidBuild;
+    public bool IsMacBuild;
+    public string Email;
+    public string Password;
+    public bool RememberUserLogin;
+
+}
